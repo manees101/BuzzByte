33f25d2077d9faad67e8b43c9612ce431259b97b
Muhammad Anees <muhammadaneesrajpoot@gmail.com>
2024-04-20 12:28:55 +0500

    Added configurations for deployment

--- backend/index.js
+++ backend/index.js
@@ -8,12 +8,24 @@ import userRouter from "./router/userRoutes.js"
 import connectDB from "./db/connect.js"
 import cookieParser from "cookie-parser"
 import bodyParser from "body-parser"
+import cors from "cors"
 const PORT=process.env.PORT || 4000
 const DB_URI=process.env.MONGO_URI
 const app=express()
 app.use(cookieParser())
 app.use(express.json())
 app.use(bodyParser.json())
+var whitelist = ['https://buzz-byte.netlify.app', 'https://buzz-byte.vercel.app']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
 app.get('/',(req,res)=>{
     res.json("Hellow from server")
 })
