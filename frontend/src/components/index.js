8db1481ad0249dcbae7a4561cfff28af9575d55c
Muhammad Anees <muhammadaneesrajpoot@gmail.com>
2024-04-20 11:45:42 +0500

    Added configurations for deployment

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 import Footer from "./Footer"
 import PostCard from "./PostCard"
 import AuthorCard from "./AuthorCard";
-import HeadPost from "./Headpost";
+import HeadPost from "./HeadPost";
 export {
     Navbar,
     Footer,
